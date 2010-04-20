bcf736ceada976252ec07b6b08f6de28630e70d4
NandaKishore <madhav.bnk@gmail.com>
2010-04-20 09:03:41 +0530

    Feed page. FeedEntry page. Minimal Search in place

--- feeds/urls.py
+++ feeds/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('feeds.views',
     (r'^$', 'view_all_feeds', {'all_feeds_template':'all_feeds_page.html'}, 'all_feeds'),
     (r'^search/$', 'view_search', {'search_template':'search.html'}, 'search'),
+    (r'^(?P<feed_id>\d+)/$', 'view_feed_profile', {'feed_profile_template':'feed_profile.html'}, 'feed_profile'),
     (r'^(?P<feedentry_id>\d+)/(?P<feedentry_slug>[\w-]+)/$', 'view_feedentry_profile', {'feedentry_profile_template':'feedentry_profile.html'}, 'feedentry_profile'),
 )
