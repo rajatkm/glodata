f425908a50ef2f17a9fe49ef2a15de24ddec98cf
Nanda Kishore <madhav.bnk@gmail.com>
2010-05-02 00:28:30 +0530

    UserFeedEntry 'name' renamed and remapped to UserProfile

--- dashboards/models.py
+++ dashboards/models.py
@@ -1,12 +1,13 @@
 from utils.models import BaseModel
 from django.db import models
 from feeds.models import FeedEntry
+from users.models import UserProfile
 
 class UserFeedEntryManager(models.Manager):
-    def create_user_feedentry(self, feedentry):
+    def create_user_feedentry(self, user, feedentry):
         user_feedentry = self.exists(feedentry = feedentry)
         if not user_feedentry:
-            user_feedentry = UserFeedEntry(name='Thilak', feedentry=feedentry)
+            user_feedentry = UserFeedEntry(user=user, feedentry=feedentry)
             user_feedentry.save()
         return user_feedentry
 
@@ -17,7 +18,7 @@ class UserFeedEntryManager(models.Manager):
             return None
 
 class UserFeedEntry(BaseModel):
-    name = models.CharField(max_length=50)
+    user = models.ForeignKey(UserProfile)
     feedentry = models.ForeignKey(FeedEntry)
     objects = UserFeedEntryManager()
 
